5bfc2555cb0b8ecd9053c056d9f148909b97f98e
sinisaV <sinisa.vucetic@student.um.si>
2023-10-10 16:17:55 +0200

    added pizza spawn

--- core/src/com/mygdx/game/MyFirstGame.java
+++ core/src/com/mygdx/game/MyFirstGame.java
@@ -21,20 +21,26 @@ public class MyFirstGame extends ApplicationAdapter {
 	private Texture backgroundImg;
 	private Texture backpackImg;
 	private Texture dumbbellImg;
+	private Texture pizzaImg;
 
 	private BitmapFont font;
 
 	private Rectangle backpack;
 	private int health;
 	private Array<Rectangle> dumbbells;
+	private Array<Rectangle> pizzas;
 
 	private float dumbbellSpawnTime;
 	private int dumbbellsCollected;
 
+	private float pizzaSpawnTime;
 
 	private static final float BACKPACK_SPEED = 250f;
 	private static final float DUMBBELL_SPEED = 300f;
 	private static final float  DUMBBELL_SPAWN_TIME = 0.5f;
+	private static final float PIZZA_SPEED = 150f;
+	private static final float PIZZA_DAMAGE = 25f;
+	private static final float PIZZA_SPAWN_TIME = 2f;
 	
 	@Override
 	public void create () {
@@ -43,7 +49,7 @@ public class MyFirstGame extends ApplicationAdapter {
 		backgroundImg = new Texture("images/fitness_background2.jpg");
 		backpackImg = new Texture("images/small_backpack.png");
 		dumbbellImg = new Texture("images/small_dumbbell.png");
-
+		pizzaImg = new Texture("images/small_pizza.png");
 		font = new BitmapFont(Gdx.files.internal("fonts/oswald-32.fnt"));
 
 		backpack = new Rectangle();
@@ -54,15 +60,21 @@ public class MyFirstGame extends ApplicationAdapter {
 
 		dumbbells = new Array<>();
 		dumbbellsCollected = 0;
-		spawnDumbbells();
+		spawnDumbbell();
+
+		pizzas = new Array<>();
+		health = 100;
+		spawnPizza();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0.5f, 0, 1);
 
-		handleInput();
-		update(Gdx.graphics.getDeltaTime());
+		if (health > 0) {
+			handleInput();
+			update(Gdx.graphics.getDeltaTime());
+		}
 
 		batch.begin();
 		draw();
@@ -76,7 +88,8 @@ public class MyFirstGame extends ApplicationAdapter {
 
 	private void update(float delta) {
 		float elapsedTime = (TimeUtils.nanosToMillis(TimeUtils.nanoTime()) / 1000f);
-		if (elapsedTime - dumbbellSpawnTime > DUMBBELL_SPAWN_TIME) spawnDumbbells();
+		if (elapsedTime - dumbbellSpawnTime > DUMBBELL_SPAWN_TIME) spawnDumbbell();
+		if (elapsedTime - pizzaSpawnTime > PIZZA_SPAWN_TIME) spawnPizza();
 
 		for (Iterator<Rectangle> it = dumbbells.iterator(); it.hasNext(); ) {
 			Rectangle coin = it.next();
@@ -90,15 +103,53 @@ public class MyFirstGame extends ApplicationAdapter {
 				it.remove();
 			}
 		}
+
+		for (Iterator<Rectangle> it = pizzas.iterator(); it.hasNext(); ) {
+			Rectangle hammer = it.next();
+			hammer.y -= PIZZA_SPEED * delta;
+			if (hammer.y + pizzaImg.getHeight() < 0) {
+				it.remove();
+			}
+			if (hammer.overlaps(backpack)) {
+				health -= PIZZA_DAMAGE;
+				//piggyVoice.play();
+				it.remove();
+			}
+		}
 	}
 
 	private void draw() {
 		batch.draw(backgroundImg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(backpackImg, backpack.x, backpack.y-15);
+
+		if (health <= 0) {
+			font.setColor(Color.RED);
+			font.draw(batch,
+					"GAME OVER",
+					20f, Gdx.graphics.getHeight() - 20f
+			);
+			font.setColor(Color.NAVY);
+			font.draw(batch,
+					"YOUR IMPROVED FITNESS HEALTH: " + (dumbbellsCollected),
+					20f, Gdx.graphics.getHeight() - 60f
+			);
+			return;
+		}
 
 		for (Rectangle dumbbell : dumbbells) {
 			batch.draw(dumbbellImg, dumbbell.x, dumbbell.y);
 		}
+
+		for (Rectangle pizza : pizzas) {
+			batch.draw(pizzaImg, pizza.x, pizza.y);
+		}
+
+		batch.draw(backpackImg, backpack.x, backpack.y-15);
+
+		font.setColor(Color.RED);
+		font.draw(batch,
+				"HEALTH: " + health,
+				20f, Gdx.graphics.getHeight() - 20f
+		);
 
 		font.setColor(Color.NAVY);
 		font.draw(batch,
@@ -119,7 +170,7 @@ public class MyFirstGame extends ApplicationAdapter {
 			backpack.x = Gdx.graphics.getWidth() - backpackImg.getWidth();
 	}
 
-	private void spawnDumbbells() {
+	private void spawnDumbbell() {
 		Rectangle dumbbell = new Rectangle();
 		dumbbell.x = MathUtils.random(0f, Gdx.graphics.getWidth() - backpackImg.getWidth());
 		dumbbell.y = Gdx.graphics.getHeight();
@@ -128,6 +179,16 @@ public class MyFirstGame extends ApplicationAdapter {
 		dumbbells.add(dumbbell);
 		dumbbellSpawnTime = TimeUtils.nanosToMillis(TimeUtils.nanoTime()) / 1000f;
 	}
+
+	private void spawnPizza() {
+		Rectangle pizza = new Rectangle();
+		pizza.x = MathUtils.random(0f, Gdx.graphics.getWidth() - pizzaImg.getWidth());
+		pizza.y = Gdx.graphics.getHeight();
+		pizza.width = pizzaImg.getWidth();
+		pizza.height = pizzaImg.getHeight();
+		pizzas.add(pizza);
+		pizzaSpawnTime = TimeUtils.nanosToMillis(TimeUtils.nanoTime()) / 1000f;
+	}
 	
 	@Override
 	public void dispose () {
@@ -135,5 +196,6 @@ public class MyFirstGame extends ApplicationAdapter {
 		font.dispose();
 		backpackImg.dispose();
 		dumbbellImg.dispose();
+		pizzaImg.dispose();
 	}
 }
